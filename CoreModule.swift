689199484079cacd2199572b21fd37560c810bd7
Jones <jones@m2pfintech.com>
2023-10-29 22:05:49 +0530

    first commit

---
+++ CoreModule.swift
@@ -0,0 +1,7 @@
+import Foundation
+import MValidator
+
+public func validate() {
+    print("========== >")
+    MValidator.validate()
+}
---
+++ MValidator.framework/Headers/MValidator.h
@@ -0,0 +1,18 @@
+//
+//  MValidator.h
+//  MValidator
+//
+//  Created by JONES JOSEPH on 24/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MValidator.
+FOUNDATION_EXPORT double MValidatorVersionNumber;
+
+//! Project version string for MValidator.
+FOUNDATION_EXPORT const unsigned char MValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MValidator/PublicHeader.h>
+
+
